c5f3a6a88eef6e3f3bc61af231e050ea33acdabb
Raptor_854 <113599350+MatthewMacDonald-1@users.noreply.github.com>
2023-11-26 17:03:19 +1300

    Some more functionality. Still need to fix fs

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "stdint.h"
 
 #define FRAMES_PER_SECOND 30
-#define UP_SCALE_IMAGES false
 #define PANORAMA_MIN_ASPECT_RATIO 21 / 9
 
 int main(void) {
@@ -16,22 +15,26 @@ int main(void) {
     SetExitKey(0);
     // raylib initialization end
 
-    const uint32_t testImagesLen = 3;
+    const int32_t testImagesLen = 5;
     char* testImages[] = {
-        "test1.png",
+        "test3.jpg",
         "test2.jpg",
-        "test3.jpg"
+        "test1.jpg",
+        "test4.jpg",
+        "test5.png"
     };
 
-    uint32_t index = 0;
+    int32_t index = 0;
     uint32_t maxFrameCount = FRAMES_PER_SECOND * 1.5;
     uint32_t frameCount = 0;
 
     float panoramaXOffset = 0;
     float panoramaBreak = 0.25;
-    bool panForPanorama = true;
 
+    bool panForPanorama = true;
     bool fullScreenMode = false;
+    bool showIndex = false;
+    bool blowUpImages = false;
 
     uint32_t standardMaxFrameCount = FRAMES_PER_SECOND * 1.5;
     uint32_t panoramaMaxFrameCount = FRAMES_PER_SECOND * 5;
@@ -47,12 +50,38 @@ int main(void) {
     {
         if (IsKeyPressed(KEY_ESCAPE)) pauseMenu = !pauseMenu;
 
+        if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_LEFT)) {
+            if (IsKeyPressed(KEY_LEFT)) {
+                index --;
+            } else {
+                index ++;
+            }
+            if (index >= testImagesLen) index = 0;
+            if (index < 0) index = testImagesLen - 1;
+            frameCount = 0;
+            displayImage = LoadImage(testImages[index]);
+            displayImageTexture = LoadTextureFromImage(displayImage);
+            float aspectRatio = displayImageTexture.width / displayImageTexture.height;
+            if (aspectRatio > PANORAMA_MIN_ASPECT_RATIO && panForPanorama) {
+                maxFrameCount = panoramaMaxFrameCount;
+                panoramaXOffset = 0;
+            } else {
+                maxFrameCount = standardMaxFrameCount;
+            }
+            UnloadImage(displayImage);
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
         float xScale = (float)GetScreenWidth() / (float)displayImageTexture.width;
         float yScale = (float)GetScreenHeight() / (float)displayImageTexture.height;
         float scale = min(xScale, yScale);
         float aspectRatio = displayImageTexture.width / displayImageTexture.height;
+
+        if (scale > 1 && !blowUpImages) {
+            scale = 1;
+            yScale = 1;
+        }
 
         if (aspectRatio > PANORAMA_MIN_ASPECT_RATIO && panForPanorama) {
             if (frameCount > FRAMES_PER_SECOND * panoramaBreak && frameCount < (maxFrameCount - FRAMES_PER_SECOND * panoramaBreak)) {
@@ -65,6 +94,11 @@ int main(void) {
             DrawTextureRelativeEx(displayImageTexture, (Vector2){0,0}, 0, scale, Center, Center, WHITE);
         }
 
+        if (showIndex) {
+            char* text = TextFormat("%d / %d", index + 1, testImagesLen);
+            DrawTextRelativeEx(GetFontDefault(), text, (Vector2){10, -5}, BottomLeft, BottomLeft, 20, 1, RAYWHITE);
+        }
+
 
         if (!pauseMenu) {
             frameCount++;
@@ -74,6 +108,7 @@ int main(void) {
                 if (index >= testImagesLen)
                     index = 0;
                 displayImage = LoadImage(testImages[index]);
+                
                 displayImageTexture = LoadTextureFromImage(displayImage);
                 float aspectRatio = displayImageTexture.width / displayImageTexture.height;
                 if (aspectRatio > PANORAMA_MIN_ASPECT_RATIO && panForPanorama) {
@@ -87,7 +122,7 @@ int main(void) {
         } else {
             DrawRectangleRelative((Vector2){0,0}, (Vector2){GetScreenWidth(), GetScreenHeight()}, Center, Center, (Color){0,0,0,100});
             int buttonHeight = 32;
-            int yStart = -64;
+            int yStart = -128;
             int yOffset = buttonHeight * 1.25;
             int yIndex = 0;
             int padding = 20;
@@ -113,11 +148,30 @@ int main(void) {
                 } else {
                     SetWindowState(FLAG_WINDOW_MAXIMIZED);
                     SetWindowState(FLAG_FULLSCREEN_MODE);
-                    // ToggleFullscreen();
                     fullScreenMode = true;
                 }
-                // ToggleFullscreen();
             }
+
+            if (!blowUpImages) {
+                if (GuiButtonRelative("Enlarge Small Images", (Vector2){0, yStart + yIndex++ * yOffset}, (Vector2){MeasureText("Enlarge Small Images", 20) + padding, buttonHeight}, Center, TopCenter, 20)) {
+                    blowUpImages = true;
+                }
+            } else {
+                if (GuiButtonRelative("Don't enlarge Small Images", (Vector2){0, yStart + yIndex++ * yOffset}, (Vector2){MeasureText("Don't enlarge Small Images", 20) + padding, buttonHeight}, Center, TopCenter, 20)) {
+                    blowUpImages = false;
+                }
+            }
+
+            if (showIndex) {
+                if (GuiButtonRelative("Hide Index", (Vector2){0, yStart + yIndex++ * yOffset}, (Vector2){MeasureText("Hide Index", 20) + padding, buttonHeight}, Center, TopCenter, 20)) {
+                    showIndex = false;
+                }
+            } else {
+                if (GuiButtonRelative("Show Index", (Vector2){0, yStart + yIndex++ * yOffset}, (Vector2){MeasureText("Show Index", 20) + padding, buttonHeight}, Center, TopCenter, 20)) {
+                    showIndex = true;
+                }
+            }
+            
 
             if (GuiButtonRelative("Exit to Desktop", (Vector2){0, yStart + yIndex++ * yOffset}, (Vector2){MeasureText("Exit to Desktop", 20) + padding, buttonHeight}, Center, TopCenter, 20)) break;
         }
